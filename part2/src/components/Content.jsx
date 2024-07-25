429417a38834e31099a48963fd415dbd74e80f76
Martin Henchoz <mhenchoz@yahoo.com>
2024-07-25 16:00:18 -0300

    Part2-1

---
+++ part2/src/components/Content.jsx
@@ -0,0 +1,15 @@
+import Part from "./Part"
+
+const Content = ({parts}) => {
+    return (
+        <div>
+            {
+                parts.map(part => 
+                    <Part key={part.id} name={part.name} />
+                )
+            }
+        </div>
+    )
+}
+  
+export default Content  
---
+++ part2/src/components/Part.jsx
@@ -0,0 +1,7 @@
+const Part = ({name}) => {
+    return (
+        <h4>{name}</h4>    
+    )
+}
+  
+export default Part  
