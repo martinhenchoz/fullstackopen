b9a9ad05ab41f3ae81f9425205428328327a4472
Martin Henchoz <mhenchoz@yahoo.com>
2024-08-10 00:05:45 -0300

    uibuild

--- part3/part3-notes-backend/index.js
+++ part3/part3-notes-backend/index.js
@@ -67,10 +67,6 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
